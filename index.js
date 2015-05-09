78836476bc866d3ab5120f530f084273cc125c34
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-05-09 19:51:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-document.getElementById('_gibResult').innerHMTL = "Write some text to get started :D";
-setInterval(function(){
-  document.getElementById('_gibResult').innerHMTL = 'This text has a chance of ' + gibberish.detect(document.getElementById('textEntry').value) + '% of being gibberish.';
-}, 500);
+window.onload = function(){
+  document.getElementById('_gibberishDetectedResult').innerHMTL = "Write some text to get started :D";
+
+  setInterval(function(){
+    document.getElementById('_gibberishDetectedResult').innerHMTL = 'This text has a chance of ' + gibberish.detect(document.getElementById('textEntry').value) + '% of being gibberish.';
+  }, 500);
+}
