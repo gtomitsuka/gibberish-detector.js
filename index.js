e87e580a54d698d415b4c4c93e5164aa08ea21aa
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-05-09 19:49:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert(document.getElementById('_gibResult').innerHMTL) = "Write some text to get started :D";
+document.getElementById('_gibResult').innerHMTL = "Write some text to get started :D";
 setInterval(function(){
   document.getElementById('_gibResult').innerHMTL = 'This text has a chance of ' + gibberish.detect(document.getElementById('textEntry').value) + '% of being gibberish.';
 }, 500);
