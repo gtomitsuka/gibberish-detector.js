b35f9e9f1838ce5edb05c0d113ae046d9232525c
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-05-09 19:34:39 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('result').innerHMTL = "Write some text to get started :D"
+setInterval(function(){
+  document.getElementById('result').innerHMTL = 'This text has a chance of ' + gibberish.detect(document.getElementById('textEntry').value) + '% of being gibberish.';
+}, 500);
